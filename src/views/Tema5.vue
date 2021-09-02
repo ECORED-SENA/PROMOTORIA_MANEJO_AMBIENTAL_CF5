411cbd8f84d54913a6de1feb197f5b1d0fe382a1
luisEfe80 <luismendez80@hotmail.com>
2021-09-02 06:57:39 -0400

    Adicion de extra pages

---
+++ src/views/Tema5.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 5
+      h1 Análisis de ciclo de vida
+
+
+ 
+
+      
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/Tema6.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 6
+      h1 Documentación para implementar los impactos ambientales
+
+
+ 
+
+      
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
