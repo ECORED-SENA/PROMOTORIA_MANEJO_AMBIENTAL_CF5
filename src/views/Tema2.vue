7b8201efab6193dd49255d68c67b565e1183ff34
luisEfe80 <luismendez80@hotmail.com>
2021-09-02 15:24:06 -0400

    Adicion de imagenes y contenido

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -54,8 +54,8 @@
                   i.fas.fa-check
                   | Procesos de obtención de energía, que liberan gases contaminantes.
             .col-md-5
-            figure
-              img.r10(src='@/assets/curso/t2-sl2.jpg', alt='imagen decorativa')
+              figure
+                img.r10(src='@/assets/curso/t2-sl2.jpg', alt='imagen decorativa')
 
           .row.justify-content-center
             .col-md-6.mb-4.mb-md-0
@@ -147,7 +147,6 @@
               figure
                 img.r10(src='@/assets/curso/t2-sl12.jpg', alt='imagen decorativa')         
 
-
           .row.justify-content-center
             .col-md-6.mb-4.mb-md-0
               .h4 Contaminación genética
@@ -186,18 +185,35 @@
 
                 
 
-
-
-
-
-
-
-
     Separador
     br
     br
     #t_2_2.titulo-segundo
       h2 2.2. Medición de los diferentes tipos de contaminantes    
+    .row.mb-5
+      .col-lg-7
+        p.mb-3  La contaminación, en cualquier de sus tipos es un problema grave para los seres vivos, y desde todas las perspectivas se pretende minimizar las consecuencias sanitarias y socioeconómicas que se derivan de tener una escasa calidad del medio ambiente de manera general. 
+        p.mb-0.text-bold  Atendiendo a esto, es fundamental conocer las mediciones que existen de los diferentes tipos de contaminantes, a partir del siguiente recurso:
+      .col-lg-5
+        figure
+          img.r10(src='@/assets/curso/t2-i1.jpg', alt='Botellas de plastico flotando en el mar')  
+    
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .tarjeta.color-primario.p-4.mb-5
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-1
+              figure
+                img.nW(src="@/assets/curso/t2-ic2.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h3.mb-1 Medición de los diferentes tipos de contaminantes
+                  p.text-small A continuación, se ha diseñado una guía sobre los diferentes tipos de contaminantes. Descarga la siguiente infografía.
+                .col-sm-auto
+                  a.boton.color-acento-botones.texto-blanco(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+                    span Descargar
+                    i.fas.fa-file-download
 
 
     Separador
@@ -205,15 +221,128 @@
     br
     #t_2_3.titulo-segundo
       h2 2.3. Clasificación de los contaminantes 
+    .row.mb-5
+      .col-lg-2
+        figure
+          img.nW(src="@/assets/curso/t2-ic3.svg")
+      .col-lg-10
+        p.mb-0  Los contaminantes pueden llegar al ecosistema de diferentes formas que tienen efectos negativos en el medio ambiente y la salud de las personas y animales, los cuales se pueden clasificar entre primario y secundarios, pero, además, se identifica según el tipo de fuente, es decir, de manera natural o antropogénica, como se podrá conocer a continuación:
+    
+    //-- Aqui vienne los custom cards
+
+    p.mb-5  De acuerdo con lo anterior, a continuación, se relacionan los tipos de fuentes:
+    
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img.nW(src="@/assets/curso/t2-ic6.svg")
+            .col-lg-8
+              .bloque-texto-a__texto.p-4(data-aos="flip-down")
+                .h4 Contaminación natural
+                p.mb-3 La contaminación natural es la que se produce por procesos propios de la naturaleza y puede llegar a afectar el aire, suelo, agua. Esta se puede identificar por medio de las lluvias que se introducen en la tierra, también por medio de la erupción de un volcán o cuando la materia orgánica se descompone y emite ciertas partículas que pueden afectar el estado natural de la tierra. 
+                p.mb-3  Algunos de estos contaminantes se relacionan a continuación: 
+    TabsA.color-acento-contenido.mb-5
+      .tarjeta.bg-soft-green.p-4(titulo="Erupciones volcánicas")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i2.jpg")
+          .col-lg-8
+            .h4 Erupciones volcánicas
+            p Cuando un volcán entra en el proceso de erupción, se liberan diversas sustancias como el dióxido de carbono, azufre, flúor o cloro, que llegan a la atmósfera, el suelo y el agua. Estos materiales se pueden encontrar en una variedad de estados, incluidos líquidos, sólidos y gases. 
+
+      .tarjeta.bg-soft-green.p-4(titulo="Incendios forestales")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i3.jpg")
+          .col-lg-8
+            .h4 Incendios forestales 
+            p Ocurren de forma natural, pero los incendios forestales también causan contaminación. La quema de una vasta vegetación, humo, dióxido de carbono y monóxido de carbono libera cenizas y otros elementos a la atmósfera, contaminando primero la atmósfera, luego el suelo y el agua.
 
 
+      .tarjeta.bg-soft-green.p-4(titulo="Corrientes de aire")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i4.jpg")
+          .col-lg-8
+            .h4 Corrientes de aire  
+            p El viento contiene polvo, semillas, esporas, polen, sustancias y partículas de un lugar a otro, lo cual altera el equilibrio de cada ecosistema afectado.
+
+      .tarjeta.bg-soft-green.p-4(titulo="Hongos")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i5.jpg")
+          .col-lg-8
+            .h4 Hongos
+            p Son organismos que emiten gases, que pueden ser fatales para los humanos. En el ambiente natural, las concentraciones de estos gases son inofensivas, pero pueden ocurrir en ambientes pequeños y húmedos como los sótanos.
+
+    //-- Aqui vienen los otros
 
 
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img.nW(src="@/assets/curso/t2-ic7.svg")
+            .col-lg-8
+              .bloque-texto-a__texto.p-4(data-aos="flip-down")
+                .h4 Contaminación antropogénica
+                p.mb-3 Son todas las actividades humanas que tienen cierto grado de afectación sobre el medio ambiente. La principal causa de la contaminación por el hombre es el consumo excesivo y el estilo de vida urbano. 
+    TabsA.color-acento-contenido.mb-5
+      .tarjeta.bg-soft-green.p-4(titulo="Actividad industrial")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i6.jpg")
+          .col-lg-8
+            .h4 Actividad industrial
+            p Este tipo de contaminación se da principalmente por la descarga de contaminantes tóxicos al medio ambiente sin control de calidad, composición y densidad química. La quema a gran escala de combustibles fósiles como el petróleo, el carbón y el gas, así como las aguas residuales contaminadas que contaminan la tierra, los ríos y las lagunas son las principales causas de la contaminación industrial.
+
+      .tarjeta.bg-soft-green.p-4(titulo="Actividad agrícola")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i7.jpg")
+          .col-lg-8
+            .h4 Actividad agrícola
+            p El uso de pesticidas, fertilizantes y otras sustancias, puede generar elementos altamente tóxicos en el agua y el suelo, los cuales son utilizados en los procesos para cultivo de las tierras, los cuales contaminan el ecosistema.
 
 
+      .tarjeta.bg-soft-green.p-4(titulo="Minería")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i8.jpg")
+          .col-lg-8
+            .h4 Minería
+            p Es uno de los principales responsables de la contaminación ya que para poder realizar la extracción de ciertos minerales y materiales se requiere del uso de sustancias y técnicas que contaminan el ecosistema. Un ejemplo de ello es el mercurio que llega a fuentes de agua subterráneas.
 
+      .tarjeta.bg-soft-green.p-4(titulo="Transporte")
+        .row
+          .col-lg-4
+            figure
+              img.nW.r10(src="@/assets/curso/t2-i5.jpg")
+          .col-lg-8
+            .h4 Transporte
+            p Los combustibles fósiles permiten que se movilicen los diferentes vehículos, además para la construcción de vías, se deben destruir extensiones de bosques, así como la reconducción de los cuerpos de agua para la construcción de estas.
 
-
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-acento-contenido.p-4(data-aos="flip-down")
+          .row.align-items-center
+            .col-lg-2
+              figure
+                img.nW.r10(src="@/assets/curso/t2-ic8.svg")
+            .col-lg-10
+              p.mb-0  Con la identificación de los principales problemas medioambientales, es posible hacer que las empresas se percatan de su propio alcance a la influencia dentro de estas problemáticas, permitiendo que se desenfoquen en minimizar sus impactos mediante el análisis de las actividades y procesos empleados, creando a su vez conciencia y aclarándose como una prioridad al interior de las empresas.
 
 
 
