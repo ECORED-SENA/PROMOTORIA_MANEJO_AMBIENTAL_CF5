c4edbc27659b528602b629dab4597a2d4901581f
luisEfe80 <luismendez80@hotmail.com>
2021-09-02 13:05:24 -0400

    Adicion de contenido

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,10 +7,191 @@
       .titulo-principal__numero
         span 2
       h1 Fuentes de contaminación
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0(data-aos="fade-left") Las técnicas de documentación consisten en la identificación, recogida y análisis de documentos relacionados con el hecho o contexto estudiado.
+            .col-lg-8
+              .bloque-texto-a__texto.p-4(data-aos="flip-down")
+                p La contaminación ambiental está arrasando de manera desmesurada con el medio ambiente y esto va en aumento junto con el desarrollo social. Los cambios físicos, químicos o biológicos que puedan afectar la salud, el bienestar o la supervivencia de la biodiversidad están directamente relacionados con las fuentes de contaminación. Es por esto por lo que se determinan los siguientes tipos de contaminantes: 
 
 
     #t_2_1.titulo-segundo
       h2 2.1. Tipos de contaminantes
+    p.mb-5  Teniendo en cuenta las fuentes de contaminación, es importante, además, determinar los tipos de contaminantes que afectan el ambiente, como se relacionan a continuación:
+    .tarjeta.bg-soft-green.neg-margin-x.r-0
+      .p-5
+        SlyderA
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación del agua
+              p Afecta de manera directa a los animales, las especies de hierbas y también a los humanos, pues asume la contaminación de varias fuentes de agua, que influye en los ríos, las fuentes de agua subterránea, los lagos y el mar cuando se libera la contaminación de residuos, y el agua potable se convierte en un recurso, que no es adecuado para el consumo.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl1.jpg', alt='imagen decorativa')
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación del aire
+              p.mb-4 Se produce a causa de la emisión de sustancias químicas a la atmósfera las cuales afectan la calidad del aire, afectando toda la vida que habita en el planeta. Las causas más relevantes para este tipo de contaminación son:
+              ul.lista-ul
+                li
+                  i.fas.fa-check
+                  | Gases expulsados por los motores de combustión.
+                li
+                  i.fas.fa-check
+                  | Gases como el dióxido de azufre o el monóxido de carbono, emitidos por fábricas.
+                li
+                  i.fas.fa-check
+                  | Desastres naturales como los incendios, que liberan elevadas cantidades de dióxido de carbono.
+                li
+                  i.fas.fa-check
+                  | Quema de materiales de desecho.
+                li
+                  i.fas.fa-check
+                  | Procesos de obtención de energía, que liberan gases contaminantes.
+            .col-md-5
+            figure
+              img.r10(src='@/assets/curso/t2-sl2.jpg', alt='imagen decorativa')
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación del suelo
+              p Se produce mediante el uso de sustancias químicas de uso común en la agricultura, como pesticidas, herbicidas o insecticidas que afectan su calidad. Las principales afectaciones se dan con las plantas y los animales que se nutren por tierra, también lleva a la ausencia de las primeras debido a la erosión del suelo. 
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl3.jpg', alt='imagen decorativa')
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación térmica
+              p Se lleva a cabo como resultado de las modificaciones en el clima global, en particular mediante un aumento de la temperatura. La modificación se produce de manera artificial por la emisión de gases o partículas, por lo que está estrechamente asociado con la contaminación del aire.  
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl4.jpg', alt='imagen decorativa')
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación radioactiva
+              p Se genera mediante la liberación de sustancias que suspenden los residuos radiactivos con el medio como los rayos beta. Las causas se deben a la liberación de residuos radiactivos de centrales nucleares, daños a los reactores nucleares o el uso de armas nucleares. Es una de las contaminaciones más dañinas para el ser vivo, pues afecta de manera directa el ADN, lo que lleva a que se produzca malformaciones graves y mutaciones, incluidas las enfermedades.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl5.jpg', alt='imagen decorativa')                
+                  
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación acústica
+              p Es la presencia de ruido o vibración en la atmósfera. En esta contaminación no se tiene en cuenta el tipo de emisor acústico del cual proviene. Generalmente afecta a las personas en el desarrollo de su actividad o para los activos de toda la naturaleza.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl6.jpg', alt='imagen decorativa')                
+           
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación lumínica
+              p Se produce especialmente en las ciudades, como consecuencia de las actividades humanas y pueden producir enfermedades oculares, dolores de cabeza, migrañas, afecta a hábitos alimentarios y reproductivos, migraciones o bienestar en general.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl7.jpg', alt='imagen decorativa') 
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación electromagnética
+              p Se deben a las emisiones electromagnéticas generadas por uno o más luces de faro, las mismas o diferentes frecuencias. Estas son frecuentes debido a la televisión, la radio, la telefonía o las líneas eléctricas emiten campos electromagnéticos en diferentes frecuencias, y se agregan sus efectos, causando el riesgo con un punto grande.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl8.jpg', alt='imagen decorativa') 
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación visual
+              p Impacta sobre la observación o visualización de un ambiente determinado, rompiendo con la estética de un paisaje o entorno natural. 
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl9.jpg', alt='imagen decorativa') 
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación alimentaria
+              p Se genera por agentes tóxicos y contaminantes que entran al cuerpo, ya sean por consumo de productos naturales como procesados. 
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl0.jpg', alt='imagen decorativa') 
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación microbiológica
+              p Se da por microbios o contaminación biológica se puede dar en cualquier medio, el suelo, el agua, los alimentos, etc. Sucede cuando organismos microscópicos invaden dicha zona de forma que la dañan.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl11.jpg', alt='imagen decorativa')            
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación química
+              p Es la introducción de elementos y sustancias industriales, en los tejidos orgánicos y ecosistemas, ocasionando cambios imprevisibles y a menudo tóxicos o letales, como consecuencia de reacciones químicas descontroladas.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl12.jpg', alt='imagen decorativa')         
+
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación genética
+              p Es la inclusión en el medio ambiente de genes que interactúan y compiten con la genética establecida de las especies como plantas y animales. 
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl13.jpg', alt='imagen decorativa')      
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación por basura
+              p Implica daños al suelo, aire y agua por la acumulación de residuos no deseados. Esta basura generalmente está conformada por desechos depositados de forma incorrecta y concentra tanto residuos sólidos, como líquidos.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl14.jpg', alt='imagen decorativa')     
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación por clorofluorocarburos
+              p Son responsables de un gran agujero en la capa de ozono que hay sobre el Hemisferio Norte y la Antártica, generalmente los CFC no se destruyen en la atmósfera baja, así que logran subir hasta la estratósfera donde se acumulan y destruyen la capa de ozono. 
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl15.jpg', alt='imagen decorativa')     
+
+          .row.justify-content-center
+            .col-md-6.mb-4.mb-md-0
+              .h4 Contaminación por plásticos
+              p Es uno de los principales problemas medioambientales de la actualidad. Cada año, hay un promedio de ocho (8) millones de toneladas vertidas a los océanos, afectando a todo tipo de animales marinos.
+          
+            .col-md-5
+              figure
+                img.r10(src='@/assets/curso/t2-sl16.jpg', alt='imagen decorativa')  
+
+                
+
+
+
+
+
+
+
 
     Separador
     br
@@ -18,7 +199,7 @@
     #t_2_2.titulo-segundo
       h2 2.2. Medición de los diferentes tipos de contaminantes    
 
-      
+
     Separador
     br
     br
