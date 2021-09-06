f91ed5ec54ad6e8af07366054b4145d6f4349f0e
luisEfe80 <luismendez80@hotmail.com>
2021-09-06 16:14:59 -0400

    Adicion de contenido

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -17,41 +17,180 @@
         figure(data-aos="flip-left")
           img.r10(src='@/assets/curso/t4-i1.jpg', alt='imagen decorativa')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .tarjeta--container.row.mb-5
+      .col-md.c2.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/t4-ic1.svg', alt='icono decorativo')
+          
+        h3.text-center El aprovechamiento de recursos naturales ya sean renovables
+        p Tales como el aprovechamiento forestal o la pesca; o no renovables, tales como la extracción del petróleo o del carbón.
+      .col-md.c1.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/t4-ic2.svg', alt='icono decorativo')
+          
+        h3.text-center Contaminación
+        p Todos los proyectos que producen algún residuo emiten gases a la atmósfera o vierten líquidos al ambiente.
+      .col-md.c2.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/t4-ic3.svg', alt='icono decorativo')
+          
+        h3.text-center Ocupación del territorio
+        p Los proyectos que al ocupar un territorio modifican las condiciones naturales por acciones tales como desmonte, compactación del suelo y otras
 
 
 
     #t_4_1.titulo-segundo
       h2 4.1. Clasificación de los impactos ambientales
+    p.mb-5  Dentro de los impactos ambientales, existen diversas clasificaciones de acuerdo con sus atributos; por ejemplo:
+    .row.mb-4.justify-content-center
+      .col-lg-10
+        PasosA.color-acento-contenido.mb-5(tipo="n")
+          .row
+            .col-md-2
+              figure
+                img.nW(src='@/assets/curso/t4-ic4.svg', alt='icono decorativo')
+            .col-md-6.mb-4.mb-md-0
+              h3 Positivo o negativo
+              p En términos del efecto resultante en el ambiente.
+            
+
+          .row.justify-content-right
+            .col-md-10
+              h3 Directo o indirecto
+              p Si es causado por alguna acción del proyecto o es resultado del efecto producido por la acción.
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img.nW(src='@/assets/curso/t4-ic5.svg', alt='icono decorativo')    
+
+          .row
+            .col-md-2
+              figure
+                img.nW(src='@/assets/curso/t4-ic6.svg', alt='icono decorativo')
+            .col-md-6.mb-4.mb-md-0
+              h3 Acumulativo
+              p Es el resultado de la suma de impactos ambientales que ocurrieron en tiempo pasado o que aún están pasando en el presente.   
+            
+
+          .row.justify-content-right
+            .col-md-10
+              h3 Sinérgico
+              p Se produce cuando el efecto conjunto de impactos supone una incidencia mayor que la suma de los impactos individuales.
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img.nW(src='@/assets/curso/t4-ic7.svg', alt='icono decorativo')    
+
+          .row
+            .col-md-2
+              figure
+                img.nW(src='@/assets/curso/t4-ic8.svg', alt='icono decorativo')
+            .col-md-6.mb-4.mb-md-0
+              h3 Residual
+              p El que persiste después de la aplicación de medidas de mitigación.  
+            
+
+          .row.justify-content-right
+            .col-md-10
+              h3 Temporal o permanente
+              p Si por un período determinado o es definitivo.
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img.nW(src='@/assets/curso/t4-ic9.svg', alt='icono decorativo')    
+
+          .row
+            .col-md-2
+              figure
+                img.nW(src='@/assets/curso/t4-ic10.svg', alt='icono decorativo')
+            .col-md-6.mb-4.mb-md-0
+              h3 Reversible o irreversible
+              p Dependiendo de la posibilidad de regresar a las condiciones originales.
+            
+
+          .row.justify-content-right
+            .col-md-10
+              h3 Continuo o periódico
+              p Dependiendo del período en que se manifieste.
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img.nW(src='@/assets/curso/t4-ic11.svg', alt='icono decorativo')    
+
+
 
     Separador
     br
     br
     #t_4_2.titulo-segundo
       h2 4.2. Identificación del impacto ambiental    
+    p.mb-5  Para identificar los impactos ambientales es necesario conocer primero los aspectos ambientales dentro de su organización, tanto de las actividades, productos o servicios y cómo estos afectan el medio ambiente. A continuación, se relacionan algunas variables que se deben tener como factor contaminante:
+    .row.mb-5
+      .col-lg-4
+        //-- Aqui vienene los cards
 
-      
+    p.mb-5  En el impacto ambiental existen una serie de pasos fundamentales para tener en cuenta en el proceso de identificación, como lo son:
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        PasosA.color-acento-contenido.mb-5(tipo="l")
+          div
+            .h4 Declaración de los objetivos del proyecto.
+          div
+            .h4 Análisis de las posibilidades tecnológicas para lograr el objetivo.
+          div
+            .h4 Declaración de una o varias acciones propuestas, incluyendo alternativas, que puedan causar impacto ambiental.
+          div
+            .h4 Descripción de las características y condiciones del medio ambiente, antes del inicio de las actividades.
 
+          div
+            .h4 Descripción de las acciones propuestas, incluyendo un análisis de costos y beneficios.
+          div
+            .h4 Análisis de los impactos ambientales de las acciones propuestas.
+
+          div
+            .h4 Evaluación de los impactos de las acciones propuestas sobre el medio ambiente.
+
+          div
+            .h4 Resumen y recomendaciones.
+
+    .row.mb-5
+      .col-lg-2
+        figure
+          img.nW(src='@/assets/curso/t4-ic12.svg', alt='icono decorativo')    
+      .col-lg-8
+        p.mb-3.text-bold  La evaluación, al ser uno de los pasos fundamentales en el impacto ambiental, requiere conocer el proceso que se debe realizar, sobre todo al situarlo desde el manejo y utilización de la matriz Leopold:
+        p.mb-3  En la década de 1970 por el Dr. Med. Luna Leopold y algunos colaboradores desarrollaron proyectos de construcción con influencia, enfoque y contenido particularmente útiles, para la evaluación preliminar de estos proyectos que predicen un gran impacto ambiental. La forma de usar la matriz de Leopold se puede resumir en los siguientes pasos:
+    .row.mb-5.justify-content-center
+      .col-lg-8
+        ImagenInfografica.color-secundario.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/t4-info1.svg', alt='infografia')
+
+          .tarjeta.color-acento-contenido.p-3(x="26%" y="12%")
+            p Limite la esfera de influencia.
+          .tarjeta.color-acento-contenido.p-3(x="73%" y="12%")
+            p Determine si el tamaño es positivo o negativo.
+          .tarjeta.color-acento-contenido.p-3(x="89%" y="28%")
+            p Determine cuántas medidas de proyecto influyen en el medio ambiente, en la desagregación positiva y negativa.
+          .tarjeta.color-acento-contenido.p-3(x="89%" y="28%")
+            p Determine cuántas medidas de proyecto influyen en el medio ambiente, en la desagregación positiva y negativa.
+          .tarjeta.color-acento-contenido.p-3(x="95%" y="50%")
+            p Determine cuántas medidas de proyecto influyen en el medio ambiente, en la desagregación positiva y negativa.
+          .tarjeta.color-acento-contenido.p-3(x="89%" y="72%")
+            p Determine cuántos elementos del medio ambiente se ven afectados por el proyecto que se adoptan en negativos positivos y negativos.
+          .tarjeta.color-acento-contenido.p-3(x="74%" y="88%")
+            p Agregue resultados de los elementos del medio ambiente.            
+          .tarjeta.color-acento-contenido.p-3(x="26%" y="88%")
+            p Determine el tamaño de cada acción en cada elemento en una escala de 1 a 10.
+          .tarjeta.color-acento-contenido.p-3(x="11%" y="72%")
+            p Determine el significado de cada elemento en una escala de 1 a 10.
+          .tarjeta.color-acento-contenido.p-3(x="6%" y="50%")
+            p Determine para cada acción los elementos se ven afectados.
+          .tarjeta.color-acento-contenido.p-3(x="11%" y="28%")
+            p Determine las acciones que ejerce el proyecto en el área.
 
 </template>
 
